3acf7c061ac7d3741402a7c14a98955f5e12f59b
Deepak3015 <d037486511@gmail.com>
2025-01-15 16:35:11 +0530

    3rd

--- basics.py
+++ basics.py
@@ -393,6 +393,12 @@ import os
 #         print('x',end =' ')
 #     print()
 
+# r = 4
+# for i in range(1,r+1):
+#     for j in range(1,i+1):
+#         print(j,end = ' ')
+#     print()
+
     
 
 
