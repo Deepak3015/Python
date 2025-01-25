d550e70738ebd00fb9631ac92981b96aa2ccfbe7
Deepak3015 <d03748651@gmail.com>
2025-01-25 10:50:48 +0530

    2nd

--- panda.py
+++ panda.py
@@ -1,8 +1,21 @@
 import pandas as pd
  
  
-df = pd.read_csv("New folder/sales_data_sample.csv",encoding= "latin1")
+# df = pd.read_csv("New folder/sales_data_sample.csv",encoding= "latin1")
 
 
+# print(df)
+
+# Creating the data frame 
+
+data ={
+    "name": ["alice","bob","Charlie"],
+    "age": [30,40,50],
+    "city": ["newyork","losangles","lasvegas"]
+}
+df = pd.DataFrame(data)
 print(df)
+df.to_csv("First  OUtput")
+
+
 
